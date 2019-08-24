5419283b3c49e8f99ecfdd1734e3f16735272e1e
omarkhan5299 <53892435+omarkhan5299@users.noreply.github.com>
2019-08-24 10:27:08 +0530

    Update Q2a.cpp

--- Q2a.cpp
+++ Q2a.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-	int arr[10],k,n,small,temp,kt=0;
+	int arr[10],k,n,small,temp,kt=0,min_idx;
 	cout<<"Enter the size of the array:";
 	cin>>n;
 	cout<<"Enter the elements of the array:";
@@ -18,20 +18,21 @@ int main()
 	
 		
 	
-	for(int i=0;i<n-1;i++)
-	{	
+
 		
-		
-		for(int j=0;j<n-1-i;j++)
-		{
-				if(arr[j+1]<arr[j])
-				{
-						temp=arr[j];
-						arr[j]=arr[j+1];
-						arr[j+1]=temp;
-						
-				}
-		}
+		for (int i = 0; i < k; i++)  
+    {  
+        
+        min_idx = i;  
+        for (int j = i+1; j < n; j++)  
+        if (arr[j] < arr[min_idx])  
+            min_idx = j;  
+  
+       
+       
+        temp=arr[min_idx];
+        arr[min_idx]=arr[i];
+        arr[i]=temp;
 		
 					
 	}
